64b3a105c2ec5f7ee9a9cbdf00d9d79035c67db2
FabioBritto <fabio.tritono@gmail.com>
2026-02-18 18:20:29 -0300

    refactor: added validation to user/authuser dtos

---
+++ src/main/java/com/academo/controller/dtos/security/UserAuthDTO.java
@@ -0,0 +1,12 @@
+package com.academo.controller.dtos.security;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public record UserAuthDTO(
+        @Email(message = "O email informado é inválido")
+        @NotEmpty(message = "O email é obrigatório para a criação da conta")
+        String username,
+        @NotEmpty(message = "A senha é obrigatória")
+        String password) {
+}
