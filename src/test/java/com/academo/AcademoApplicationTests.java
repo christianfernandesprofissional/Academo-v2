1f4d9dd6303397cfc360330216162e995327a94b
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2026-03-07 13:16:05 -0300

    fix: corrigindo erros de build test

--- src/test/java/com/academo/AcademoApplicationTests.java
+++ src/test/java/com/academo/AcademoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.academo;
 
+import com.academo.service.storage.google.DriveService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class AcademoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private DriveService driveService;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
