1dd8036272d1151a753fb1d16594bde92451f111
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2026-03-07 10:48:48 -0300

    fix: fazendo adicionando mock de driveService no AcademoApplicationTests

--- src/test/java/com/academo/AcademoApplicationTests.java
+++ src/test/java/com/academo/AcademoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.academo;
 
+import com.academo.service.storage.google.DriveService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class AcademoApplicationTests {
 
 
-    @MockBean
-    private JavaMailSender javaMailSender;
+    @Bean
+    public DriveService driveService() {
+        return Mockito.mock(DriveService.class);
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return Mockito.mock(JavaMailSender.class);
+    }
 
 	@Test
 	void contextLoads() {
