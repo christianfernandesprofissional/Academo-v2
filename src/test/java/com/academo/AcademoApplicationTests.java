f0e4087a1610cf32032b30117f43aac3390c03d3
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2026-03-07 10:54:54 -0300

    fix: corrigindo AcademoApplicationTests

--- src/test/java/com/academo/AcademoApplicationTests.java
+++ src/test/java/com/academo/AcademoApplicationTests.java
@@ -4,6 +4,7 @@ import com.academo.service.storage.google.DriveService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,19 +12,22 @@ import org.springframework.mail.javamail.JavaMailSender;
 @SpringBootTest
 class AcademoApplicationTests {
 
-
-    @Bean
-    public DriveService driveService() {
-        return Mockito.mock(DriveService.class);
+    @Test
+    void contextLoads() {
     }
 
-    @Bean
-    public JavaMailSender javaMailSender() {
-        return Mockito.mock(JavaMailSender.class);
-    }
+    @TestConfiguration
+    static class TestConfig {
 
-	@Test
-	void contextLoads() {
-	}
+        @Bean
+        public DriveService driveService() {
+            return Mockito.mock(DriveService.class);
+        }
+
+        @Bean
+        public JavaMailSender javaMailSender() {
+            return Mockito.mock(JavaMailSender.class);
+        }
+    }
 
 }
